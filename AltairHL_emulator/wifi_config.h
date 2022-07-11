c858a2e2ad082f49854a1f71272a59a1b558e88e
Dave Glover <dglover@microsoft.com>
2022-07-11 15:55:00 +1000

    Wifi config from SD Card

--- AltairHL_emulator/wifi_config.h
+++ AltairHL_emulator/wifi_config.h
@@ -3,7 +3,9 @@
 #include "../IntercoreContract/intercore_contract.h"
 #include "dx_intercore.h"
 #include "dx_utilities.h"
+#include "parson.h"
 #include <applibs/log.h>
+#include <applibs/wificonfig.h>
 
 extern DX_INTERCORE_BINDING intercore_sd_card_ctx;
 extern INTERCORE_DISK_DATA_BLOCK_T intercore_disk_block;
