6230aff2621efaa1684fb7584454a718dd40f83a
Dave Glover <dglover@microsoft.com>
2022-08-01 14:39:52 +1000

    version bump

--- AltairHL_emulator/main.h
+++ AltairHL_emulator/main.h
@@ -39,7 +39,7 @@
 #include "io_ports.h"
 #include "memory.h"
 
-const char ALTAIR_EMULATOR_VERSION[]   = "4.6.9";
+const char ALTAIR_EMULATOR_VERSION[]   = "4.7.0";
 const char DEFAULT_NETWORK_INTERFACE[] = "wlan0";
 #define Log_Debug(f_, ...)      dx_Log_Debug((f_), ##__VA_ARGS__)
 #define DX_LOGGING_ENABLED      FALSE
