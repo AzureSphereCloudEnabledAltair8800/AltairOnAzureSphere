55732e40ccc97969f9a81db0f965a3177093d890
Dave Glover <dglover@microsoft.com>
2022-08-03 23:16:24 +1000

    SD with cache - experimental

--- AltairRT_sd_card_service_cache/main.c
+++ AltairRT_sd_card_service_cache/main.c
@@ -30,9 +30,15 @@
 #include "Socket.h"
 #include "intercore_contract.h"
 #include "SD.h"
+#include "uthash.h"
 
-static SPIMaster* driver = NULL;
-static SDCard* card = NULL;
+// CacheEntry is 176 bytes
+// 800 items is stable
+#define MAX_CACHE_ITEMS 800
+#define SECTOR_LENGTH 137
+
+static SPIMaster *driver = NULL;
+static SDCard *card = NULL;
 static volatile int blocks_read = 0;
 static volatile int blocks_written = 0;
 
@@ -41,37 +47,89 @@ static volatile int blocks_written = 0;
 #define PAGE_SIZE MAX_WRITE_BLOCK_LEN
 uint8_t dataBlock[PAGE_SIZE];
 
-
-
 INTERCORE_DISK_DATA_BLOCK_T txStruct;
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 static const Component_Id A7ID =
-{
-    .seg_0 = 0xac8d863a,
-    .seg_1 = 0x4424,
-    .seg_2 = 0x11eb,
-    .seg_3_4 = {0xb3, 0x78, 0x02, 0x42, 0xac, 0x13, 0x00, 0x02}
-};
+    {
+        .seg_0 = 0xac8d863a,
+        .seg_1 = 0x4424,
+        .seg_2 = 0x11eb,
+        .seg_3_4 = {0xb3, 0x78, 0x02, 0x42, 0xac, 0x13, 0x00, 0x02}};
 
 static uint8_t recvBuffer[720];
 
 // Drivers
-static UART   *debug              = NULL;
-static Socket *socket             = NULL;
+static UART *debug = NULL;
+static Socket *socket = NULL;
 
 // Callbacks
-typedef struct CallbackNode {
+typedef struct CallbackNode
+{
     bool enqueued;
     struct CallbackNode *next;
     void *data;
-    void (*cb)(void*);
+    void (*cb)(void *);
 } CallbackNode;
 
 static void EnqueueCallback(CallbackNode *node);
 
-static void printSDBlock(uint8_t* buff, uintptr_t blocklen, unsigned blockID)
+struct CacheEntry
+{
+    int sector_number_key;
+    uint8_t sector[SECTOR_LENGTH];
+    UT_hash_handle hh;
+};
+
+struct CacheEntry *cache = NULL;
+
+static struct CacheEntry *find_in_cache(int sector_number_key)
+{
+    struct CacheEntry *entry;
+
+    HASH_FIND_INT(cache, &sector_number_key, entry);
+    if (entry)
+    {
+        // remove it (so the subsequent add will throw it on the front of the list)
+        HASH_DELETE(hh, cache, entry);
+        HASH_ADD_INT(cache, sector_number_key, entry);
+        return entry;
+    }
+    return NULL;
+}
+
+static void add_to_cache(int sector_number_key, uint8_t *sector)
+{
+    struct CacheEntry *entry, *tmp_entry;
+
+    HASH_FIND_INT(cache, &sector_number_key, entry); /* id already in the hash? */
+    if (entry == NULL)
+    {
+        entry = malloc(sizeof(struct CacheEntry));
+        if (entry == NULL){
+            return;
+        }
+        entry->sector_number_key = sector_number_key;
+
+        HASH_ADD_INT(cache, sector_number_key, entry);
+    }
+
+    memcpy(entry->sector, sector, SECTOR_LENGTH);
+
+    if (HASH_COUNT(cache) >= MAX_CACHE_ITEMS)
+    {
+        HASH_ITER(hh, cache, entry, tmp_entry)
+        {
+            // prune the first entry (loop is based on insertion order so this deletes the oldest item)
+            HASH_DELETE(hh, cache, entry);
+            free(entry);
+            break;
+        }
+    }
+}
+
+static void printSDBlock(uint8_t *buff, uintptr_t blocklen, unsigned blockID)
 {
     uint8_t ascBuffer[17];
     int byteCount = 0;
@@ -81,7 +139,8 @@ static void printSDBlock(uint8_t* buff, uintptr_t blocklen, unsigned blockID)
 
     __builtin_memset(ascBuffer, 0x00, sizeof(ascBuffer));
 
-    for (i = 0; i < blocklen; i++) {
+    for (i = 0; i < blocklen; i++)
+    {
         UART_PrintHexWidth(debug, buff[i], 2);
         if (buff[i] > 0x20 && buff[i] < 0x80)
         {
@@ -96,26 +155,27 @@ static void printSDBlock(uint8_t* buff, uintptr_t blocklen, unsigned blockID)
         byteCount++;
         ascBuffer[byteCount] = 0x00;
 
-        if (byteCount == sizeof(ascBuffer)-1)
+        if (byteCount == sizeof(ascBuffer) - 1)
         {
             UART_Printf(debug, "    %s\r\n", ascBuffer);
             byteCount = 0;
         }
     }
-    if ((blocklen % (sizeof(ascBuffer) - 1)) != 0) {
+    if ((blocklen % (sizeof(ascBuffer) - 1)) != 0)
+    {
         UART_Print(debug, "\r\n");
     }
     UART_Print(debug, "\r\n");
 }
 
-static void WriteDataToA7(volatile INTERCORE_DISK_DATA_BLOCK_T* data, ssize_t size)
+static void WriteDataToA7(volatile INTERCORE_DISK_DATA_BLOCK_T *data, ssize_t size)
 {
 #ifdef SHOW_DEBUG_INFO
     UART_Printf(debug, "Sending %d bytes to the A7\r\n", size);
     printSDBlock(&txStruct, sizeof(txStruct), txStruct.blockNumber);
 #endif
 
-    int32_t error = Socket_Write(socket, &A7ID, (INTERCORE_DISK_DATA_BLOCK_T * )data, size);
+    int32_t error = Socket_Write(socket, &A7ID, (INTERCORE_DISK_DATA_BLOCK_T *)data, size);
     if (error != 0)
     {
         UART_Printf(debug, "Error Result: %li\r\n", error);
@@ -124,17 +184,20 @@ static void WriteDataToA7(volatile INTERCORE_DISK_DATA_BLOCK_T* data, ssize_t si
 
 static void handleRecvMsg(void *handle)
 {
-    Socket *socket = (Socket*)handle;
-    static uint8_t buff[MAX_WRITE_BLOCK_LEN] = { 0 };
-    //uintptr_t blocklen = SD_GetBlockLen(card);
+    Socket *socket = (Socket *)handle;
+    static uint8_t buff[MAX_WRITE_BLOCK_LEN] = {0};
+    // uintptr_t blocklen = SD_GetBlockLen(card);
+    struct CacheEntry *entry;
 
     Component_Id senderId;
 
-    if (Socket_NegotiationPending(socket)) {
+    if (Socket_NegotiationPending(socket))
+    {
         UART_Printf(debug, "Negotiation pending, attempting renegotiation\r\n");
         // NB: this is blocking, if you want to protect against hanging,
         //     add a timeout
-        if (Socket_Negotiate(socket) != ERROR_NONE) {
+        if (Socket_Negotiate(socket) != ERROR_NONE)
+        {
             UART_Printf(debug, "ERROR: renegotiating socket connection\r\n");
             return;
         }
@@ -143,7 +206,8 @@ static void handleRecvMsg(void *handle)
     uint32_t size = sizeof(recvBuffer);
     int32_t error = Socket_Read(socket, &senderId, &recvBuffer[0], &size);
 
-    if (error != ERROR_NONE) {
+    if (error != ERROR_NONE)
+    {
         UART_Printf(debug, "ERROR: receiving msg - %ld\r\n", error);
         return;
     }
@@ -152,7 +216,7 @@ static void handleRecvMsg(void *handle)
     UART_Printf(debug, "Received %d bytes\r\n", size);
 #endif
 
-    INTERCORE_DISK_DATA_BLOCK_T* pMsg = (INTERCORE_DISK_DATA_BLOCK_T*)&recvBuffer[0];
+    INTERCORE_DISK_DATA_BLOCK_T *pMsg = (INTERCORE_DISK_DATA_BLOCK_T *)&recvBuffer[0];
 
 #ifdef SHOW_DEBUG_INFO
     printSDBlock(pMsg->blockData, size, pMsg->blockNumber);
@@ -161,20 +225,34 @@ static void handleRecvMsg(void *handle)
     switch (pMsg->disk_ic_msg_type)
     {
     case DISK_IC_READ:
+
         blocks_read++;
         memset(buff, 0x00, sizeof(buff));
-        if (!SD_ReadBlock(card, (uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)), buff)) {
-            // If read block fails then return an error state to the A7
-            pMsg->disk_ic_msg_type = DISK_IC_READ;
-            pMsg->success = false;
-            memset(pMsg->sector, 0x00, 137);
-            WriteDataToA7(pMsg, sizeof(INTERCORE_DISK_DATA_BLOCK_T));
+
+        entry = find_in_cache((uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)));
+        if (entry == NULL)
+        {
+            if (!SD_ReadBlock(card, (uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)), buff))
+            {
+                // If read block fails then return an error state to the A7
+                pMsg->disk_ic_msg_type = DISK_IC_READ;
+                pMsg->success = false;
+                memset(pMsg->sector, 0x00, 137);
+                WriteDataToA7(pMsg, sizeof(INTERCORE_DISK_DATA_BLOCK_T));
+            }
+            else
+            {
+                // read successful, return the block to the A7
+                // printSDBlock(dataBlock, PAGE_SIZE, pMsg->blockNumber);
+                memcpy(pMsg->sector, buff, 137);
+                pMsg->disk_ic_msg_type = DISK_IC_READ;
+                pMsg->success = true;
+                WriteDataToA7(pMsg, sizeof(INTERCORE_DISK_DATA_BLOCK_T));
+            }
         }
         else
         {
-            // read successful, return the block to the A7
-            // printSDBlock(dataBlock, PAGE_SIZE, pMsg->blockNumber);
-            memcpy(pMsg->sector, buff, 137);
+            memcpy(pMsg->sector, entry->sector, 137);
             pMsg->disk_ic_msg_type = DISK_IC_READ;
             pMsg->success = true;
             WriteDataToA7(pMsg, sizeof(INTERCORE_DISK_DATA_BLOCK_T));
@@ -184,30 +262,40 @@ static void handleRecvMsg(void *handle)
     case DISK_IC_WRITE:
         blocks_written++;
         memset(buff, 0x00, sizeof(buff));
-        memcpy(buff, pMsg->sector, 137);
+        memcpy(buff, pMsg->sector, 137);        
 
-        if (!SD_WriteBlock(card, (uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)), buff)) {
+        if (!SD_WriteBlock(card, (uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)), buff))
+        {
             // write block failed, return error state to the A7
             pMsg->success = false;
         }
         else
-        {
+        {            
             // write block success, confirm result to the A7
             pMsg->success = true;
         }
+
         pMsg->disk_ic_msg_type = DISK_IC_WRITE;
         WriteDataToA7(pMsg, sizeof(INTERCORE_DISK_DATA_BLOCK_T));
+
+        if (pMsg->success){
+            add_to_cache((uint32_t)(pMsg->sector_number + (pMsg->drive_number * 3000)), pMsg->sector);
+        }
+
+        break;
+    case DISK_IC_CLEAR:
         break;
     case DISK_IC_UNKNOWN:
         break;
     }
- }
+}
 
 static void handleRecvMsgWrapper(Socket *handle)
 {
     static CallbackNode cbn = {.enqueued = false, .cb = handleRecvMsg, .data = NULL};
 
-    if (!cbn.data) {
+    if (!cbn.data)
+    {
         cbn.data = handle;
     }
 
@@ -219,7 +307,8 @@ static CallbackNode *volatile callbacks = NULL;
 static void EnqueueCallback(CallbackNode *node)
 {
     uint32_t prevBasePri = NVIC_BlockIRQs();
-    if (!node->enqueued) {
+    if (!node->enqueued)
+    {
         CallbackNode *prevHead = callbacks;
         node->enqueued = true;
         callbacks = node;
@@ -231,16 +320,19 @@ static void EnqueueCallback(CallbackNode *node)
 static void InvokeCallbacks(void)
 {
     CallbackNode *node;
-    do {
+    do
+    {
         uint32_t prevBasePri = NVIC_BlockIRQs();
         node = callbacks;
-        if (node) {
+        if (node)
+        {
             node->enqueued = false;
             callbacks = node->next;
         }
         NVIC_RestoreIRQs(prevBasePri);
 
-        if (node) {
+        if (node)
+        {
             (node->cb)(node->data);
         }
     } while (node);
@@ -253,8 +345,9 @@ void SpiSDTest(void)
 {
     for (int x = 0; x < 10; x++)
     {
-        if (!SD_ReadBlock(card, x, &dataBlock[0])) {
-            UART_Printf(debug, "ERROR: reading block %d\r\n",x);
+        if (!SD_ReadBlock(card, x, &dataBlock[0]))
+        {
+            UART_Printf(debug, "ERROR: reading block %d\r\n", x);
             break;
         }
         else
@@ -270,22 +363,22 @@ _Noreturn void RTCoreMain(void)
     VectorTableInit();
     CPUFreq_Set(197600000);
 
-    //debug = UART_Open(MT3620_UNIT_UART_DEBUG, 115200, UART_PARITY_NONE, 1, NULL);
-    //UART_Print(debug, "\033[2J\033[0;0H--------------------------------\r\n");
-    //UART_Print(debug, "M4 SD Card Interface Application\r\n");
-    //UART_Print(debug, "App built on: " __DATE__ " " __TIME__ "\r\n");
-
+    // debug = UART_Open(MT3620_UNIT_UART_DEBUG, 115200, UART_PARITY_NONE, 1, NULL);
+    // UART_Print(debug, "\033[2J\033[0;0H--------------------------------\r\n");
+    // UART_Print(debug, "M4 SD Card Interface Application\r\n");
+    // UART_Print(debug, "App built on: " __DATE__ " " __TIME__ "\r\n");
 
     /***********************************************************************
-    * The app_manifest must also be updated
-    * SOCKET 1: Avnet Rev 1 - ISU1, CS 0 = SPIMaster_Open(MT3620_UNIT_ISU1) and SPIMaster_Select(driver, 0);
-    * SOCKET 1: Avnet Rev 2 = ISU0, CS 0 = SPIMaster_Open(MT3620_UNIT_ISU0) and SPIMaster_Select(driver, 0);
-    ***********************************************************************/
+     * The app_manifest must also be updated
+     * SOCKET 1: Avnet Rev 1 - ISU1, CS 0 = SPIMaster_Open(MT3620_UNIT_ISU1) and SPIMaster_Select(driver, 0);
+     * SOCKET 1: Avnet Rev 2 = ISU0, CS 0 = SPIMaster_Open(MT3620_UNIT_ISU0) and SPIMaster_Select(driver, 0);
+     ***********************************************************************/
 
-    driver = SPIMaster_Open(MT3620_UNIT_ISU1);
-    if (!driver) {
+    driver = SPIMaster_Open(MT3620_UNIT_ISU0);
+    if (!driver)
+    {
         UART_Print(debug,
-            "ERROR: SPI initialisation failed\r\n");
+                   "ERROR: SPI initialisation failed\r\n");
     }
     SPIMaster_DMAEnable(driver, false);
 
@@ -293,23 +386,26 @@ _Noreturn void RTCoreMain(void)
     SPIMaster_Select(driver, 0);
 
     card = SD_Open(driver);
-    if (!card) {
+    if (!card)
+    {
         UART_Print(debug,
-            "ERROR: Failed to open SD card.\r\n");
+                   "ERROR: Failed to open SD card.\r\n");
     }
 
     // Setup socket
     socket = Socket_Open(handleRecvMsgWrapper);
-    if (!socket) {
+    if (!socket)
+    {
         UART_Printf(debug, "ERROR: socket initialisation failed\r\n");
     }
 
-    //SD_SetBlockLen(card, 137);
+    // SD_SetBlockLen(card, 137);
 
     // SPI/SD test - read and display the first 10 SD Card blocks
     // SpiSDTest();
 
-    for (;;) {
+    for (;;)
+    {
         __asm__("wfi");
         InvokeCallbacks();
     }
